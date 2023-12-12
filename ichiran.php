7b1e5959a614b6b2a7b23adac6c23589e7af2aad
develophiro <abc.umd.first1@gmail.com>
2023-12-12 22:18:37 +0900

    phppush2

---
+++ ichiran.php
@@ -0,0 +1,39 @@
+<!DOCTYPE HTML PUBLIC"-//W3C//DTD HTML 4.01 Transitional//EN">
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+<title>PHP基礎</title>
+</head>
+<body>
+
+<?php
+$dsn = 'mysql:dbname=phpkiso;host=localhost';
+$user = 'root';
+$password ='';
+$dbh = new PDO($dsn,$user,$password);
+$dbh->query('SET NAMES utf8');
+
+
+$sql = 'SELECT * FROM anketo WHERE 1';
+$stmt = $dbh->prepare($sql);
+$stmt->execute();
+
+while(1)
+{
+    $rec = $stmt->fetch(PDO::FETCH_ASSOC);
+    if($rec==false)
+    {
+        break;
+    }
+    print $rec['code'];
+    print $rec['nickname'];
+    print $rec['email'];
+    print $rec['goiken'];
+    print'<br />';
+}
+
+$dbh = null;
+?>
+
+</body>
+</html>
